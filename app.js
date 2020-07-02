c89b14d09c773c901c79fd147ea13b8523da2730
Maksym Shokin <maksym.sh@upplabs.com>
2020-07-02 22:38:29 +0300

    cors-headers

--- app.js
+++ app.js
@@ -8,6 +8,16 @@ const app = express();
 
 app.use(bodyParser.json()); // parsing json requests
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PATCH, PUT, DELETE'
+  );
+  res.setHeader('Access-Control-Allow-Headers', '*');
+  next();
+});
+
 app.use('/feed', feedRoutes);
 
 app.listen('8080');
