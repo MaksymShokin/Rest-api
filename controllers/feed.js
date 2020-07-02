4f6971c272afc7fc6d417fda1c04863947d02722
Maksym Shokin <maksym.sh@upplabs.com>
2020-07-02 20:13:52 +0300

    post-route

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,3 +1,16 @@
 exports.getPosts = (req, res, next) => {
-  
-}
+  res.status(200).json({
+    posts: [{ title: 'Hello REST API', content: 'bla bla' }]
+  });
+};
+
+exports.postPosts = (req, res, text) => {
+  const title = req.body.title;
+  const content = req.body.content;
+
+  // create post in db
+  res.status(201).json({
+    message: 'Success',
+    post: { id: Math.random(), title: title, content: content }
+  });
+};
