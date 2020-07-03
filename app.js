bc0eb65d97103554ae590801af1662e1530711cb
Maksym Shokin <maksym.sh@upplabs.com>
2020-07-03 13:26:40 +0300

    post-feed

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use((req, res, next) => {
     'Access-Control-Allow-Methods',
     'GET, POST, PATCH, PUT, DELETE'
   );
-  res.setHeader('Access-Control-Allow-Headers', '*');
+  res.setHeader('Access-Control-Allow-Headers', '*'); //Content-type Authorization
   next();
 });
 
--- controllers/feed.js
+++ controllers/feed.js
@@ -1,6 +1,14 @@
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
-    posts: [{ title: 'Hello REST API', content: 'bla bla' }]
+    posts: [
+      {
+        title: 'Hello REST API',
+        content: 'bla bla',
+        creator: { name: 'Maksym' },
+        image: 'images/MyAppCost.png',
+        createdAt: new Date()
+      }
+    ]
   });
 };
 
@@ -11,6 +19,12 @@ exports.postPosts = (req, res, text) => {
   // create post in db
   res.status(201).json({
     message: 'Success',
-    post: { id: Math.random(), title: title, content: content }
+    post: {
+      _id: Math.random(),
+      title: title,
+      content: content,
+      creator: { name: 'Maksym' },
+      createdAt: new Date()
+    }
   });
 };
