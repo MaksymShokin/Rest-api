d0cb722be739615c8b332f3fe7a6dea08e601e38
Maksym Shokin <maksym.sh@upplabs.com>
2020-07-08 13:03:52 +0300

    clearing-user-posts

--- controllers/feed.js
+++ controllers/feed.js
@@ -143,6 +143,12 @@ exports.updatePost = (req, res, next) => {
         throw error;
       }
 
+      if (post.creator.toString() !== req.userId) {
+        const error = new Error('Not authorized');
+        error.statusCode = 403;
+        throw error;
+      }
+
       if (imageUrl !== post.imageUrl) {
         clearImage(post.imageUrl);
       }
@@ -175,10 +181,23 @@ exports.deletePost = (req, res, next) => {
         throw error;
       }
 
+      if (post.creator.toString() !== req.userId) {
+        const error = new Error('Not authorized');
+        error.statusCode = 403;
+        throw error;
+      }
+
       clearImage(post.imageUrl);
       return Post.findByIdAndRemove(postId);
     })
     .then(() => {
+      return User.findById(req.userId);
+    })
+    .then(user => {
+      user.posts.pull(postId);
+      return user.save()
+    })
+    .then(result => {
       res.status(200).json({ message: 'Deleted succesfully' });
     })
     .catch(err => {
